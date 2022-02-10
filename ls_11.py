312b93a374ef0da5e6a6f9076bc292b591116df0
Vitalii-Kryshtak <97515062+Vitalii-Kryshtak@users.noreply.github.com>
2022-02-10 14:58:50 +0200

    Update ls_11.py

--- ls_11.py
+++ ls_11.py
@@ -85,5 +85,4 @@ print(pupil_in_school) #164
 #Создайте словарь, где ключами являются числа, а значениями – строки.
 #Создайте функционал которий вернет новый словарь, "обратный" исходному
 dict_num_alpha = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k'}
-dict_alpha_num = {value : key for key, value in dict_num_alpha.items()}
-#
+dict_alpha_num = {value : key for key, value in dict_num_alpha.items()}#

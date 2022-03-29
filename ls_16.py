6a1ad5a205f05d30bb70d3a85801523685c25b5d
Vitalii-Kryshtak <97515062+Vitalii-Kryshtak@users.noreply.github.com>
2022-03-29 17:29:22 +0300

    Update ls_16.py

--- ls_16.py
+++ ls_16.py
@@ -16,3 +16,4 @@ def print_argument(*args, **kwargs):
 
 if __name__ == '__main__':
     print_argument(1, 2, 3, [6,8,8],(5,7,5), name='John', sex='Male', age=[3,5,2,3,4,5], level=(44,23,12,44,15,23))
+    
